8cc223dcb0e4f143df84c246d7661e2b5eddd844
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-09-13 23:09:23 -0300

    sending swagger file

---
+++ routes/swagger.js
@@ -0,0 +1,8 @@
+//routes/swagger.js
+
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger.json');
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerFile));
+module.exports = router;
