bf12d721d81649ed0862c239c1e74503da529b5f
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-09-13 22:28:59 -0300

    changing the swagger.js

--- swagger.js
+++ swagger.js
@@ -2,14 +2,25 @@
 
 const swaggerAutogen = require("swagger-autogen")();
 
+//const doc = {
+//  info: {
+//    title: "User Api",
+//   description: "User Api",
+// },
+//  host: "localhost:3000",
+//  schemes: ["http", "https"],
+//};
+
 const doc = {
   info: {
-    title: "User Api",
-    description: "User Api",
+    title: 'My API',
+    description: 'Contacts API'
   },
-  host: "localhost:3000",
-  schemes: ["http", "https"],
+  host: 'cse-341-project1.onrender.com',
+  schemes: ['https']
 };
+
+
 const outputFile = "./swagger.json";
 const endpointsFiles = ["./server.js"];
 
