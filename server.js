44c98f0f1d785181292ce71d4c69d579bfe5f1a8
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-09-13 22:10:53 -0300

    adding cors configuration

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-//server.js
+// server.js
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,14 +13,18 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes'));
 
-mongodb.initDb((err)=> {
-    if(err) {
+mongodb.initDb((err) => {
+    if (err) {
         console.log(err);
     } else {
-        app.listen(port, () => {console.log(`Connected to DB and listening on ${port}`)});
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on ${port}`);
+        });
     }
-})
+});
